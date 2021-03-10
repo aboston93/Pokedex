6a6b0f32fddde84641cf4171d3b7f1f2100f45a8
kaoko <kaoko28@gmail.com>
2021-03-09 23:39:05 -0500

    adding the styling to the pokecard as well as mounting the image in state

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,1 +1,6 @@
 //styling the surrounding card as it should apper in the pokedex
+// gonna use react router  styling to be determined
+
+import React, { Component } from 'react'
+import styled from 'styled-components'
+import {Link} from 'react-router-dom'
