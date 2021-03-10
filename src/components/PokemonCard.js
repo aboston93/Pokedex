326d123cf1effab823103c300977f69c10503c01
kaoko <kaoko28@gmail.com>
2021-03-09 23:31:46 -0500

    adding the pokemon data using axios call

---
+++ src/components/PokemonCard.js
@@ -0,0 +1,2 @@
+//styling the surrounding card
+
---
+++ src/components/pokemon/Pokemon.js
@@ -0,0 +1,306 @@
+// building the pokemon information first 
+
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+
+
+
+
+// First i need to store all of the pokemon info gonna go ahead and store it in the state 
+export default class Pokemon extends Component {
+state= {
+name:'',
+pokemonIndex:'',
+imageUrl:'',
+backimageUrl:'',
+types: [ ] ,
+description:'',
+stats: {
+    hp:'',attack:'',defense:'',specialDefense:'',speciaAttack:'',speed:''
+},
+abilities:''
+
+
+
+
+
+
+
+
+
+};
+
+
+
+
+async componentDidMount(){
+//Fleshing out the pokwmon 
+// storing the url to help with the call and the species from the api
+
+const {pokemonIndex} = this.props.match.params;
+const pokeUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+const pokeSpecies = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`
+
+//axios call for the spefic pokemon
+const pokeRes =  await axios.get(pokeUrl);
+
+const name = pokeRes.data.name;
+const imageUrl = pokeRes.data.sprites.front_default
+const backimageUrl = pokeRes.data.sprites.back_default
+
+
+
+let {hp,attack,defense,specialDefense,speciaAttack,speed} = ''
+
+
+
+const abilities = pokeRes.data.abilities.map(ability => {
+
+return ability.ability.name.toLowerCase().split('-').map(s => s.charAt(0).toUpperCase()+s.substring(1)).join
+
+})
+
+await axios.get(pokeSpecies).then(res => {
+    let description = '';
+    res.data.flavor_text_entries.some(flavor => {
+if (flavor.language.name==='en'){
+
+    description = flavor.flavor_text;
+    return;
+}
+
+    })
+    this.setState({
+    name,
+    pokemonIndex,
+    imageUrl,
+    backimageUrl,
+    description,
+    stats: {
+    hp,attack,defense,specialDefense,speciaAttack,speed
+},
+    abilities
+    
+});
+    
+})
+
+
+
+}
+
+
+//pokemon INFO styling
+    render() {
+        return (
+            <div className="col">
+        <div className="card">
+          <div className="card-header">
+            <div className="row">
+              <div className="col-5">
+                <h5>{this.state.pokemonIndex}</h5>
+              </div>
+              <div className="col-7">
+                <div className="float-right">
+                  {this.state.types.map(type => (
+                    <span
+                      key={type}
+                      className="badge badge-pill mr-1"
+                      style={{
+                        backgroundColor: `#green`,
+                        color: 'white'
+                      }}
+                    >
+                      {type
+                        .toLowerCase()
+                        .split(' ')
+                        .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+                        .join(' ')}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card-body">
+            <div className="row align-items-center">
+              <div className=" col-md-3 ">
+                <img
+                  src={this.state.imageUrl}
+                  className="card-img-top rounded mx-auto mt-2"
+                />
+              </div>
+              <div className="col-md-9">
+                <h4 className="mx-auto">
+                  {this.state.name
+                    .toLowerCase()
+                    .split(' ')
+                    .map(s => s.charAt(0).toUpperCase() + s.substring(1))
+                    .join(' ')}
+                </h4>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    HP
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar "
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.hp}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow="25"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.hp}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Attack
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.attack}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow="25"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.attack}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Defense
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar "
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.defense}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow="25"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.defense}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Speed
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar"
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.speed}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow="25"
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.speed}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Sp Atk
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar "
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.specialAttack}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow={this.state.stats.specialAttack}
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.specialAttack}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row align-items-center">
+                  <div className={`col-12 col-md-${this.state.statTitleWidth}`}>
+                    Sp Def
+                  </div>
+                  <div className={`col-12 col-md-${this.state.statBarWidth}`}>
+                    <div className="progress">
+                      <div
+                        className="progress-bar "
+                        role="progressbar"
+                        style={{
+                          width: `${this.state.stats.specialDefense}%`,
+                          backgroundColor: `#${this.state.themeColor}`
+                        }}
+                        aria-valuenow={this.state.stats.specialDefense}
+                        aria-valuemin="0"
+                        aria-valuemax="100"
+                      >
+                        <small>{this.state.stats.specialDefense}</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row mt-1">
+              <div className="col">
+                <p className="">{this.state.description}</p>
+              </div>
+            </div>
+          </div>
+          <hr />
+          <div className="card-body">
+            <h5 class="card-title text-center">Profile</h5>
+            <div className="row">
+              <div className="col-md-6">
+                
+              </div>
+              <div className="col-md-6">
+                
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-footer text-muted">
+            Data From{' '}
+            <a href="https://pokeapi.co/" target="_blank" className="card-link">
+              PokeAPI.co
+            </a>
+          </div>
+        </div>
+     
+        )
+    }
+}
